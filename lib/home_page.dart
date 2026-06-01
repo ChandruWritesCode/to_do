7ce45de2016288487d6ed61afaa5830c47e27ac4
ChandruBtechCSE <karegoudracm@gmail.com>
2026-06-01 11:05:31 +0530

    Simple To Do app

---
+++ lib/home_page.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do/main.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _cont = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("To Do"),
+        elevation: 200,
+        actions: [
+          IconButton(
+            onPressed: () {
+              final pr = context.read<TasksProvider>();
+              for (int i = pr.checkList.length - 1; i >= 0; i--) {
+                if (pr.checkList[i]) {
+                  pr.removeTask(i);
+                }
+              }
+            },
+            icon: Icon(Icons.sync),
+          ),
+        ],
+      ),
+      floatingActionButton: SizedBox(
+        height: 50,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            shape: WidgetStatePropertyAll(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            ),
+          ),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Add Task"),
+                  content: TextField(
+                    autofocus: true,
+                    controller: _cont,
+                    decoration: InputDecoration(hint: Text("your task")),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        context.read<TasksProvider>().addTask(_cont.text);
+                        Navigator.of(context).pop();
+                        _cont.clear(); // setState
+                      },
+                      child: Text("add"),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        _cont.clear();
+                      },
+                      child: Text("cancel"),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          child: Icon(Icons.add, size: 40),
+        ),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: ListView.builder(
+          itemCount: context.watch<TasksProvider>().tasks.length,
+          itemBuilder: (context, idx) {
+            return TaskCard(idx);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class TaskCard extends StatefulWidget {
+  int idx;
+  TaskCard(this.idx, {super.key});
+  @override
+  State<TaskCard> createState() => _TaskCardState();
+}
+
+class _TaskCardState extends State<TaskCard> {
+  @override
+  Widget build(BuildContext context) {
+    // return Container(color: Colors.black, child: Text("Hello"));
+    return Card(
+      color: Theme.of(context).canvasColor,
+      elevation: 20,
+      child: Container(
+        height: 80,
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          children: [
+            Checkbox(
+              value: context.read<TasksProvider>().checkList[widget.idx],
+              onChanged: (val) {
+                setState(() {
+                  context.read<TasksProvider>().checkList[widget.idx] = !context
+                      .read<TasksProvider>()
+                      .checkList[widget.idx];
+                });
+              },
+            ),
+            Text(
+              context.read<TasksProvider>().tasks[widget.idx],
+              style: TextStyle(
+                fontSize: 20,
+                decorationThickness: 2,
+                decoration: context.read<TasksProvider>().checkList[widget.idx]
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do/home_page.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => TasksProvider())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          colorSchemeSeed: Colors.amber,
+        ),
+        home: HomePage(),
+      ),
+    );
+  }
+}
+
+class TasksProvider extends ChangeNotifier {
+  List<String> tasks = [];
+  List<bool> checkList = [];
+
+  void addTask(String task) {
+    tasks.add(task);
+    checkList.add(false);
+    notifyListeners();
+  }
+
+  void toggleTask(int idx) {
+    checkList[idx] = !checkList[idx];
+    notifyListeners();
+  }
+
+  void removeTask(int idx) {
+    tasks.removeAt(idx);
+    checkList.removeAt(idx);
+    notifyListeners();
+  }
+}
